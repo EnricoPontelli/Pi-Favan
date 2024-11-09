a3818252197ae963716f1fcb2b8c4cf8ec101879
lucas.oliveira.kyr <131033597+lucaskyr@users.noreply.github.com>
2024-11-09 11:51:36 -0300

    login meio

--- Frontend/my-app/src/app/page.tsx
+++ Frontend/my-app/src/app/page.tsx
@@ -2,8 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <Image src={'/login.jpg'} alt={""} className="w-max h-max"/>
+    <div className="h-screen w-screen ">
+      <img
+      src={'/login.jpg'} alt={""} className="w-screen h-screen"/>
+      <div className="absolute top-0 w-full h-full flex items-center justify-center">
+        <div className=" bg-green-50 rounded-md bg-opacity-50 w-72 h-96 justify-center justify-items-center shadow-lg shadow-green-700/70">
+          <div className="">
+            <h1 className="text-white">Spectrasync</h1>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
